ec251d6ea1da68be917cdfbb8f439c0dc6b1eb56
Abishek <abishek0398@gmail.com>
2019-09-21 18:51:18 +0530

    Reordered structure elements to reduce padding and added additional TCB elements

--- OS/process.c
+++ OS/process.c
@@ -1,21 +1,24 @@
 #include <OS/Process.h>
 #include <OS/Queues.h>
 #include <OS/os.h>
-void os_task_create(struct tcb *temp,void(*fun_ptr)(),uint32_t **stack_base1,uint32_t stack_size,uint32_t priority)
+void os_task_create(struct tcb *temp_tcb,void(*fun_ptr)(void *args),void *args,uint32_t *stack_base,uint32_t stack_size,uint32_t priority)
 {
-	temp->next=NULL;
-	temp->prev=NULL;
-	temp->priority=priority;
-	temp->process_state=READY;
-	temp->stack_base=*stack_base1;
-	temp->stack_size=stack_size;
-	temp->program_counter=(uint32_t)fun_ptr;
-	os_stack_init(temp,stack_base1,fun_ptr);
-	start_critical();
-	add_ready_queue(temp);
-	end_critical();
+	temp_tcb->stack_base = stack_base;
+	temp_tcb->task_args = args;
+	temp_tcb->next_tcb = NULL;
+	temp_tcb->prev_tcb = NULL;
+	temp_tcb->stack_size = stack_size;
+	temp_tcb->program_counter = (uint32_t)fun_ptr;
+	temp_tcb->priority = priority;
+	temp_tcb->task_state = READY;
+	temp_tcb->task_id = 1;
+	temp_tcb->del_req = 0;
+	os_stack_init(&temp_tcb->stack_base,fun_ptr,args);
+	os_start_critical();
+	
+	os_end_critical();
 }
-void os_stack_init(struct tcb *temp,uint32_t **stack_base,void(*fun_ptr)())
+void os_stack_init(uint32_t **stack_base,void(*fun_ptr)(void *args),void *args)
 {
 	*(*stack_base)=0x01000000;
 	*(*stack_base-1)=(uint32_t)*(fun_ptr);
@@ -33,8 +36,7 @@ void os_stack_init(struct tcb *temp,uint32_t **stack_base,void(*fun_ptr)())
 	*(*stack_base-13)=0x14141414;
 	*(*stack_base-14)=0x14141414;
 	*(*stack_base-15)=0x14141414;
-	*stack_base=*stack_base-15;
-	temp->stack_base=*stack_base;
+	*stack_base = *stack_base-15;
 }
 
 	
--- include/OS/Process.h
+++ include/OS/Process.h
@@ -1,16 +1,22 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 #include <stdint.h>
-__packed struct tcb
+/* 
+    This header file is compiler dependent and is used to obtain fixed size datatypes
+*/
+struct tcb
 {
 	uint32_t *stack_base;
-	struct tcb *next;
-	struct tcb *prev;
-	uint8_t priority;
-	uint32_t process_state;     
+	uint32_t *task_args;
+	struct tcb *next_tcb;
+	struct tcb *prev_tcb;     
 	uint32_t stack_size;
 	uint32_t program_counter;
+	uint8_t priority;
+	uint8_t task_state;
+	uint8_t task_id;
+	uint8_t del_req;
 };
-void os_task_create(struct tcb *temp,void(*fun_ptr)(),uint32_t **stack_base,uint32_t stack_size,uint32_t priority);
-void os_stack_init(struct tcb *temp,uint32_t **stack_base,void(*fun_ptr)());
+extern void os_task_create(struct tcb *temp,void(*fun_ptr)(void *args),void *args,uint32_t *stack_base,uint32_t stack_size,uint32_t priority);
+extern void os_stack_init(uint32_t **stack_base,void(*fun_ptr)(void *args),void *args);
 #endif

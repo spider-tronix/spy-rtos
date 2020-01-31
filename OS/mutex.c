13f80ddd484e27262b832ff1d1ada5c347f35b31
venkatkrj <47356388+VenkateshTK@users.noreply.github.com>
2020-01-31 19:04:25 +0530

    mutex master updated

--- OS/mutex.c
+++ OS/mutex.c
@@ -1,42 +1,48 @@
 #include <OS/tasks.h>
 #include <OS/os.h>
+
 void os_mutex_create(struct mutex *mut)
 {
-	mut->lock = 0;
-	mut->mut_ptr_head = NULL;
-	mut->mut_ptr_tail = NULL;
-	mut->owner_tcb = NULL;
+
+		os_start_critical();
+		mut->lock = 1;// 1 -> indicates resource available - ok
+		os_end_critical(); // extend the critical section till the end of the function
+		mut->old_prio = NULL;
+		mut->mut_ptr_head = NULL;
+		mut->mut_ptr_tail = NULL;
+		mut->owner_tcb = NULL;
 }
 
-void os_mutex_lock(struct mutex *mut)
+void os_mutex_wait(struct mutex *mut)
 {
-	intr_alloc();
 	os_start_critical();
-	if (mut->lock == 1)
+	if(mut->lock)
 	{
-		os_block(mut);
-		if (mut->owner_tcb->priority > current_tcb->priority)
-		{
-			os_change_prio(current_tcb, mut->owner_tcb->priority);
-		}
-		os_end_critical();
-		os_scheduler();
+		mut->lock = 0; // locked
+		mut->owner_tcb = current_tcb;
+		mut->old_prio = mut->owner_tcb->priority;// logic problem fixed - ok
+		os_end_critical();	
 	}
 	else
 	{
-		mut->lock = 1;
-		mut->owner_tcb = current_tcb;
-		mut->owner_tcb->priority = mut->highest_prio;
+		if (current_tcb->priority < mut->owner_tcb->priority) // PIP implemented -ok
+		{		
+			os_change_prio(mut->owner_tcb, current_tcb->priority);
+		}
 		os_end_critical();
+		os_block(mut);
 	}
 }
 
-void os_mutex_release(struct mutex *mut)
+void os_mutex_signal(struct mutex *mut)
 {
-	intr_alloc();
 	os_start_critical();
-	if (mut->owner_tcb == current_tcb)
+	if(mut->owner_tcb == current_tcb)
 	{
-		mut->lock = 0;
+		mut->lock=1;
+		os_change_prio(mut->owner_tcb, mut->old_prio);
+		os_release(mut);
 	}
+	os_end_critical();
+	os_scheduler();
 }
